bbb21ea4b51cb719349b69c579acb903235242db
middleverse <arshi_b@hotmail.com>
2021-02-01 12:44:28 -0500

    openweathermap api working, error handling in views

--- v1/views.py
+++ v1/views.py
@@ -7,11 +7,20 @@ from django.http import HttpResponse
 API_KEY = '4fa02188ba9205a1f619645287c69336'
 
 def weather(request):
+    '''
+        Returns weather information as a JSON repsonse
+
+        Required params: City name(str)
+    '''
     if request.method == 'GET':
-        city = 'toronto'
+        try:
+            city = request.GET['city']
+        except Exception as e:
+            return HttpResponse('Search parameter ' + str(e) + ' required.' , status=202)
+        if not city:
+            return HttpResponse('Please provide a city name.', status=202)
         post_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + API_KEY
         r = requests.get(post_url)
         print(r.json)
         return HttpResponse(r.content, status=200)
     return HttpResponse('POST requests are not allowed, try GET.', status=202)
-
