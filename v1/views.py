ce20cecf812376be2bf57ef0810a68d37610a12f
middleverse <arshi_b@hotmail.com>
2021-02-01 12:31:58 -0500

    api returning unparsed json

--- v1/views.py
+++ v1/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import requests
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+API_KEY = '4fa02188ba9205a1f619645287c69336'
+
+def weather(request):
+    if request.method == 'GET':
+        city = 'toronto'
+        post_url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + API_KEY
+        r = requests.get(post_url)
+        print(r.json)
+        return HttpResponse(r.content, status=200)
+    return HttpResponse('POST requests are not allowed, try GET.', status=202)
+
