b6e8cf4058fb902510490e45ccbf6001f001ccf3
middleverse <arshi_b@hotmail.com>
2021-02-01 20:02:34 -0500

    skeleton tests added

--- v1/tests.py
+++ v1/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
+import views, requests
+
 # Create your tests here.
+class TestWeatherApiMethod(TestCase):
+    
+    def test_no_city(self):
+        pass
+
+    def test_valid_city(self):
+        pass
+
+    def test_invalid_city(self):
+        pass
+
+    def test_long_string_city(self):
+        pass
+
+    def test_integer_as_city(self):
+        pass
